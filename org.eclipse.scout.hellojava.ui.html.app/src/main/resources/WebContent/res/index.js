e659f93699e47e44fd3574ec17b08fe2a1cbe7c9
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2020-02-06 18:20:27 +0100

    hellojava.app: ES6 Mig: step 2

--- org.eclipse.scout.hellojava.ui.html.app/src/main/resources/WebContent/res/index.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  new scout.RemoteApp().init();
-});
---
+++ org.eclipse.scout.hellojava.ui.html.app/webpack.config.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) BSI Business Systems Integration AG. All rights reserved.
+ * http://www.bsiag.com/
+ */
+const baseConfig = require('@eclipse-scout/cli/scripts/webpack-defaults');
+module.exports = (env, args) => {
+  args.resDirArray = [
+    'node_modules/@eclipse-scout/core/res',
+    'src/main/resources/WebContent'];
+  const config = baseConfig(env, args);
+
+  config.entry = {
+    'hellojava': './src/main/js/hellojava.js',
+    'login': './src/main/js/login.js',
+    'logout': './src/main/js/logout.js',
+    'hellojava-theme': './src/main/js/hellojava-theme.less'
+  };
+
+  return config;
+};
