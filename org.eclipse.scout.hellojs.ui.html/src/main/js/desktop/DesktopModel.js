66c94787ae15bcdf2a532225d417634afa7eb316
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2020-11-04 16:57:05 +0100

    Use arrow function in models

--- org.eclipse.scout.hellojs.ui.html/src/main/js/desktop/DesktopModel.js
+++ org.eclipse.scout.hellojs.ui.html/src/main/js/desktop/DesktopModel.js
@@ -1,14 +1,12 @@
-export default function() {
-  return {
-    id: 'hellojs.Desktop',
-    objectType: 'Desktop',
-    navigationVisible: false,
-    navigationHandleVisible: false,
-    headerVisible: false,
-    views: [
-      {
-        objectType: 'hellojs.HelloForm'
-      }
-    ]
-  };
-}
+export default () => ({
+  id: 'hellojs.Desktop',
+  objectType: 'Desktop',
+  navigationVisible: false,
+  navigationHandleVisible: false,
+  headerVisible: false,
+  views: [
+    {
+      objectType: 'hellojs.HelloForm'
+    }
+  ]
+});
--- org.eclipse.scout.hellojs.ui.html/src/main/js/greeting/HelloFormModel.js
+++ org.eclipse.scout.hellojs.ui.html/src/main/js/greeting/HelloFormModel.js
@@ -1,49 +1,47 @@
 import {FormField} from '@eclipse-scout/core';
 
-export default function() {
-  return {
-    id: 'hellojs.HelloForm',
-    displayHint: 'view',
-    modal: false,
-    rootGroupBox: {
-      id: 'MainBox',
-      objectType: 'GroupBox',
-      fields: [
-        {
-          id: 'DetailBox',
-          objectType: 'GroupBox',
-          cssClass: 'detail-box',
-          gridColumnCount: 1,
-          gridDataHints: {
-            fillHorizontal: false,
-            fillVertical: false,
-            horizontalAlignment: 0,
-            verticalAlignment: 0,
-            widthInPixel: 450,
-            weightY: 1
+export default () => ({
+  id: 'hellojs.HelloForm',
+  displayHint: 'view',
+  modal: false,
+  rootGroupBox: {
+    id: 'MainBox',
+    objectType: 'GroupBox',
+    fields: [
+      {
+        id: 'DetailBox',
+        objectType: 'GroupBox',
+        cssClass: 'detail-box',
+        gridColumnCount: 1,
+        gridDataHints: {
+          fillHorizontal: false,
+          fillVertical: false,
+          horizontalAlignment: 0,
+          verticalAlignment: 0,
+          widthInPixel: 450,
+          weightY: 1
+        },
+        bodyLayoutConfig: {
+          vgap: 25
+        },
+        fields: [
+          {
+            id: 'NameField',
+            objectType: 'StringField',
+            label: '${textKey:Name}',
+            labelPosition: FormField.LabelPosition.TOP,
+            statusVisible: false
           },
-          bodyLayoutConfig: {
-            vgap: 25
-          },
-          fields: [
-            {
-              id: 'NameField',
-              objectType: 'StringField',
-              label: '${textKey:Name}',
-              labelPosition: FormField.LabelPosition.TOP,
-              statusVisible: false
-            },
-            {
-              id: 'GreetButton',
-              objectType: 'Button',
-              label: '${textKey:Hello}',
-              processButton: false,
-              defaultButton: true,
-              keyStroke: 'enter'
-            }
-          ]
-        }
-      ]
-    }
-  };
-}
+          {
+            id: 'GreetButton',
+            objectType: 'Button',
+            label: '${textKey:Hello}',
+            processButton: false,
+            defaultButton: true,
+            keyStroke: 'enter'
+          }
+        ]
+      }
+    ]
+  }
+});
